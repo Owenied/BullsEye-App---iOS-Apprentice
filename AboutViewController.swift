dac27281b00145a218693fda3dcbf2b201c79661
Owen Duignan <owenied@me.com>
2017-04-11 15:33:52 -0500

    Added About page complete with a close button

---
+++ AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye App - iOS Apprentice
+//
+//  Created by Owen Duignan on 11/04/2017.
+//  Copyright © 2017 Owen Duignan. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}

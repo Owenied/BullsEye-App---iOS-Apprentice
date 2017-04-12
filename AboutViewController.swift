bf37867f49a67f586fd1eb8b991cdfcdb7691bab
Owen Duignan <owenied@me.com>
2017-04-12 14:56:09 -0500

    Added webview and html doc for the About screen

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -5,12 +5,29 @@
 //  Created by Owen Duignan on 11/04/2017.
 //  Copyright © 2017 Owen Duignan. All rights reserved.
 //
-
+//
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
-    @IBAction func close() {
-        dismiss(animated: true, completion: nil)
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+       
+          if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+
+            if let htmlData = try? Data(contentsOf: url) {
+                
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
+      @IBAction func close() {
+        
+          dismiss(animated: true, completion: nil)
     }
 }
